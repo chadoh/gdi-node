ca4c38364c531d7f04bebbc20cfbdddedd8dd538
Chad Ostrowski <hi@chadoh.com>
2016-05-22 14:36:31 -0400

    Add noRoute catch-all endpoint

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var homeController = require('./controllers/home');
 var app = express();
 
 app.get('/', homeController.index);
+app.get('/*', homeController.noRoute);
 
 app.listen(3000, function () {
   console.log('Your app is listening on port 3000!');
--- controllers/home.js
+++ controllers/home.js
@@ -1,5 +1,8 @@
 module.exports = {
   index: function (req, res) {
     res.send('Welcome to the GDI Node Workshop!');
+  },
+  noRoute: function (req, res) {
+    res.send('404 Not Found');
   }
 }
