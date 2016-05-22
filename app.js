807b5327e316e22d18e36dee919b9b48bf045754
Chad Ostrowski <hi@chadoh.com>
2016-05-22 14:13:59 -0400

    Add homeController to handle logic of root path

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var express = require('express');
+var homeController = require('./controllers/home');
+
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Welcome to the GDI Node Workshop!');
-});
+app.get('/', homeController.index);
 
 app.listen(3000, function () {
   console.log('Your app is listening on port 3000!');
